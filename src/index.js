5331a7464e11b6fa259c86602c8323dfe12b57ab
id-edm <ivandubochit@gmail.com>
2025-09-04 18:19:46 +0400

    feat: ready task solution

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-/*
 const MORSE_TABLE = {
   '.-': 'a',
   '-...': 'b',
@@ -37,8 +36,27 @@ const MORSE_TABLE = {
   '----.': '9',
   '-----': '0',
 };
-*/
 
-module.exports = function decode(/* expr */) {
-  throw new Error('Not implemented');
-};
+function decode(expr) {
+  let result = '';
+  for (let i = 0; i < expr.length; i += 10) {
+    const longSymbol = expr.slice(i, i + 10);
+    if (longSymbol === '**********') {
+      result += ' ';
+    } else {
+      let morseSymbol = '';
+      for (let j = 0; j < 10; j += 2) {
+        const pair = longSymbol.slice(j, j + 2);
+        if (pair === '10') {
+          morseSymbol += '.';
+        } else if (pair === '11') {
+          morseSymbol += '-';
+        }
+      }
+      result += MORSE_TABLE[morseSymbol];
+    }
+  }
+  return result;
+}
+
+module.exports = decode;
